3451a762e8cdcf6a1f85394e62fb4c3eeb8e946c
alf130386 <35611821+alf130386@users.noreply.github.com>
2021-03-03 17:22:30 +0200

    fixed receiving universes

--- src/helper.h
+++ src/helper.h
@@ -3,9 +3,10 @@
 
 #define NUM_LEDS 840
 #define UNIVERSE_SIZE 120
-#define START_UNIVERSE 41
+#define START_UNIVERSE 0
 #define ARTNET_PORT 6454
 
+#define SHOW_DELAY 2000
 
 /* 
    * ETH_CLOCK_GPIO0_IN   - default: external clock from crystal oscillator
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,16 @@
 
 //#include <artnetESP32/ArtnetESP32.h>
 
-const uint8_t universesCount = NUM_LEDS/UNIVERSE_SIZE;
+const uint32_t universesCount = NUM_LEDS/UNIVERSE_SIZE;
 uint8_t uniData[512];
 uint8_t headerData[18]; //artnetHeader
-uint32_t syncmax,sync;
+uint32_t syncmax,sync; //for syncCount
+int16_t lostPackets = 0;
+
+//for syncTime
+bool allowShow = false;
+long lastPacketTime = 0;
+
 CRGB leds[NUM_LEDS];
 
 WiFiUDP udp;
@@ -30,17 +36,18 @@ void setup() {
       sync=0;
 }
 
-long ttime = 0;
+uint32_t unisCount = 0;
 void readUdp() {
   
   if(udp.parsePacket()) {
-    
     udp.read(headerData, 18);
     int uniSize = (headerData[16] << 8) + headerData[17];
     uint8_t universe = headerData[14];
     if(universe >= START_UNIVERSE && universe < (START_UNIVERSE + universesCount) && uniSize > 500) {
-      printf("univ: %d, time: %lumcs\n", universe, micros() - ttime);
-      ttime = micros();
+      printf("univ: %d, time: %lumcs\n", universe, micros() - lastPacketTime);
+      unisCount++;
+      lastPacketTime = micros();
+      allowShow = true;
       int offset = (universe - START_UNIVERSE)*UNIVERSE_SIZE;
       udp.read((uint8_t *)(leds+offset), UNIVERSE_SIZE*3);
       sync=sync | (1<<(universe - START_UNIVERSE));
@@ -49,19 +56,39 @@ void readUdp() {
           sync=1;
          }
           /* end of code **/
-         if(sync==syncmax) {
-           long tt1 = micros();
-           FastLED.show();
-           long tt2 = micros();
-           printf("***** show: %lu micros\n", tt2-tt1);
-           //memset(universesReceived, 0, universesCount);
-           sync=0;
-         }
+
     }
     udp.flush();
   }
 }
 
+//sends data to strips when packet for all universes received
+void showSyncCount() {
+           if(sync==syncmax) {
+           long tshow1 = micros();
+           FastLED.show();
+           long tshow2 = micros();
+           printf("***** show: %lu micros\n", tshow2-tshow1);
+           sync=0;
+         }
+}
+
+//sends data to strips after passing some time after last packet received
+void showSyncTime() {
+           if(allowShow && (micros() - lastPacketTime) > SHOW_DELAY) {
+             allowShow = false;
+           long tshow1 = micros();
+           FastLED.show();
+           long tshow2 = micros();
+           printf("diff: %d\n", universesCount-unisCount);
+             lostPackets = lostPackets + (universesCount-unisCount);
+           printf("***** show: %lu micros, lost: %d\n", tshow2-tshow1, lostPackets);
+           unisCount = 0;
+         }
+}
+
 void loop() {
   readUdp();
+  //showSyncCount();
+  showSyncTime();
 }
