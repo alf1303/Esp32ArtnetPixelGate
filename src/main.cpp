78306f2ec0ba28e79984524e02e469e9020b0889
alf130386 <35611821+alf130386@users.noreply.github.com>
2021-03-17 13:06:01 +0200

    3

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <artnetESP32/ArtnetESP32.h>
 FASTLED_USING_NAMESPACE
 
-IPAddress ipaddr = IPAddress(192,168,0,53);
+IPAddress ipaddr = IPAddress(192,168,0,47);
 IPAddress gateway = IPAddress(192,168,0,101);
 IPAddress subnet = IPAddress(255,255,255,0);
 
